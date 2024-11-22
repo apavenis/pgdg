4cd0d537c23fc04296254d12300965d93f90d68b
Devrim Gunduz <devrim@gunduz.org>
2024-11-22 19:18:27 +0300

    scripts: Also check common repo.

--- scripts/buildserver/check_upload_status.sh
+++ scripts/buildserver/check_upload_status.sh
@@ -9,6 +9,8 @@
 # Include common values:
 source ~/bin/global.sh
 
+# Check eack PostgreSQL major release:
+
 for packageBuildVersion in ${pgStableBuilds[@]}
 do
 	echo "Checking PostgreSQL $packageBuildVersion - $os"
@@ -20,3 +22,16 @@ do
 	fi
 	echo
 done
+
+# Also check the common repo:
+
+echo "Checking $os common repo:"
+upload_status=`curl -s -o /dev/null -w "%{http_code}" https://download.postgresql.org/pub/repos/yum/common/$osdistro/$os-$osarch/repodata`
+echo "Status: $upload_status"
+if [ $upload_status == "404" ]
+then
+	echo "${red}ERROR:${reset} There is a problem with the $os common repo"
+fi
+echo
+
+exit 0
